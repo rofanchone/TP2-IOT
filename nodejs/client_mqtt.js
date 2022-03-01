59a7d81fc792aa66d0c37759a0dae4290d20388d
Robin Fanchone <rofanchone@gmail.com>
2022-03-01 18:50:59 -0500

    Ajout deux capteurs

--- nodejs/client_mqtt.js
+++ nodejs/client_mqtt.js
@@ -1,6 +1,10 @@
 var mqtt = require('mqtt');
-var Topic = 'TEMPERATURE';
+var Topic1 = 'capteur1';
+var Topic2 = 'capteur2';
 var Broker_URL = 'mqtt://192.168.10.10';
+
+var capteur1;
+var capteur2;
 
 var options = {
 	clientId: 'MyMQTT',
@@ -20,12 +24,13 @@ client.on('close', mqtt_close);
 function mqtt_connect()
 {
     console.log("Connecting MQTT");
-    client.subscribe(Topic, mqtt_subscribe);
+    client.subscribe(Topic1);
+	client.subscribe(Topic2, mqtt_subscribe);
 }
 
 function mqtt_subscribe(err, granted)
 {
-    console.log("Subscribed to " + Topic);
+    console.log("Subscribed to " + Topic1 + " & " + Topic2);
     if (err) {console.log(err);}
 }
 
@@ -44,8 +49,19 @@ function mqtt_error(err)
 
 function mqtt_messsageReceived(topic, message, packet)
 {
-	console.log('Topic=' +  topic + '  Message=' + message);
-	insert(parseInt(message));
+	if (topic === 'capteur1') {
+		capteur1 = message;
+		insertCapteur1(parseFloat(message));
+		console.log('Topic=' +  topic + '  Message=' + message);
+	}
+
+	if (topic === 'capteur2') {
+		capteur2 = message;
+		insertCapteur2(parseFloat(message));
+		console.log('Topic=' +  topic + '  Message=' + message);
+	}
+	
+	
 
 }
 
@@ -54,7 +70,7 @@ function mqtt_close()
 	console.log("Close MQTT");
 }
 
-function insert(data) {
+function insertCapteur1(data, collection) {
 	var MongoClient = require('mongodb').MongoClient;
 	var url = "mongodb://192.168.10.5/";
 
@@ -64,7 +80,23 @@ function insert(data) {
 		var myobj = { temperature: data};
 		dbo.collection("capteur1").insertOne(myobj, function(err, res) {
 		  if (err) throw err;
-		  console.log("1 document inserted");
+		  console.log("Une données insérée");
+		  db.close();
+		});
+	  });
+}
+
+function insertCapteur2(data, collection) {
+	var MongoClient = require('mongodb').MongoClient;
+	var url = "mongodb://192.168.10.5/";
+
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("tp2iot");
+		var myobj = { temperature: data};
+		dbo.collection("capteur2").insertOne(myobj, function(err, res) {
+		  if (err) throw err;
+		  console.log("Une données insérée");
 		  db.close();
 		});
 	  });
