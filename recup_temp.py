1f25587e328a49060053b3daab25aa265138c520
Robin Fanchone <rofanchone@gmail.com>
2022-03-03 09:16:50 -0500

    ajout deux capteurs

--- recup_temp.py
+++ recup_temp.py
@@ -4,9 +4,19 @@ from pymodbus.client.sync import ModbusSerialClient
 import time
 import os
 
-client = ModbusSerialClient(
+capteur1 = ModbusSerialClient(
     method='rtu',
     port='/dev/ttyUSB0',
+    baudrate=57600,
+    timeout=3,
+    parity='N',
+    stopbits=1,
+    bytesize=8
+)
+
+capteur2 = ModbusSerialClient(
+    method='rtu',
+    port='/dev/ttyUSB1',
     baudrate=57600,
     timeout=3,
     parity='N',
@@ -19,27 +29,49 @@ client_mqtt = mqtt.Client("TEMPERATURE")
 client_mqtt.connect(mqttBroker, 1883)
 
 while (True):
-    if client.connect():  # Trying for connect to Modbus Server/Slave
+    if capteur1.connect():  # Trying for connect to Modbus Server/Slave
         time.sleep(2)
         '''Reading from a holding register with the below content.'''
-        res = client.read_holding_registers(address=100, count=1, unit=1)
+        res1 = capteur1.read_holding_registers(address=100, count=1, unit=1)
 
         '''Reading from a discrete register with the below content.'''
         # res = client.read_discrete_inputs(address=1, count=1, unit=1)
 
-        if not res.isError():
-            data = res.registers
-            temp = data.pop()
-            print(temp)
-            commande = "mosquitto_pub -h localhost -t TEMPERATURE -m {}".format(temp)
+        if not res1.isError():
+            data = res1.registers
+            temp1 = data.pop()
+            print(temp1)
+            commande = "mosquitto_pub -h localhost -t capteur1 -m {}".format(temp1)
             os.system(commande)
             
         else:
-            print(res)
+            print(res1)
 
     else:
-        print('Cannot connect to the Modbus Server/Slave')
+        print('(Capteur1)Cannot connect to the Modbus Server/Slave')
+        
+        
+    
+    if capteur2.connect():  # Trying for connect to Modbus Server/Slave
+        time.sleep(2)
+        '''Reading from a holding register with the below content.'''
+        res2 = capteur1.read_holding_registers(address=100, count=1, unit=1)
+
+        '''Reading from a discrete register with the below content.'''
+        # res = client.read_discrete_inputs(address=1, count=1, unit=1)
+
+        if not res2.isError():
+            data = res2.registers
+            temp2 = data.pop()
+            print(temp2)
+            commande = "mosquitto_pub -h localhost -t capteur2 -m {}".format(temp2)
+            os.system(commande)
+            
+        else:
+            print(res2)
+
+    else:
+        print('(Capteur2)Cannot connect to the Modbus Server/Slave')
     
     time.sleep(120)
     
-
